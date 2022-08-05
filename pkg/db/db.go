708df117028ef9615b82a115f84474aceafe2c7e
Shulammite-Aso <asoamarachi@gmail.com>
2022-08-05 15:49:31 -0700

    initial commit

---
+++ pkg/db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Handler struct {
+	Collection *mongo.Collection
+}
+
+func Init(url string) Handler {
+
+	mongoCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	log.Println("Connecting to MongoDB...")
+	client, err := mongo.Connect(mongoCtx, options.Client().ApplyURI(url))
+	if err != nil {
+		log.Fatalf("Error Starting MongoDB Client: %v", err)
+	}
+
+	collection := client.Database("filebox_svc").Collection("user_filebox")
+
+	return Handler{collection}
+}
---
+++ pkg/models/file_entry.go
@@ -0,0 +1,10 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type FileEntry struct {
+	Id       primitive.ObjectID `bson:"_id,omitempty"`
+	Username string             `bson:"username"`
+	FileName string             `bson:"fileName"`
+	File     []byte             `bson:"file"`
+}
